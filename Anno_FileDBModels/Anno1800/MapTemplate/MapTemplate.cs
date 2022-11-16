c693b2ee07ab1fbfb7fdae1e4824fcfc1292261e
Shad0wlife <matze_skywalker@hotmail.de>
2022-11-16 21:29:09 +0100

    Add Map Template Structure
    Credit to @jakobharder for the original structures in the Anno Map Editor that I adapted for the new FileDBSerializer and expanded where needed to work with all game files.

---
+++ Anno_FileDBModels/Anno1800/MapTemplate/MapTemplate.cs
@@ -0,0 +1,30 @@
+﻿using FileDBSerializing.ObjectSerializer;
+
+namespace Anno_FileDBModels.Anno1800.MapTemplate
+{
+    public class MapTemplate
+    {
+        public int[]? Size { get; set; }
+        public int[]? PlayableArea { get; set; }
+        public List<RandomlyPlacedThirdParty>? RandomlyPlacedThirdParties { get; set; }
+        public int? ElementCount { get; set; }
+
+        [FlatArray]
+        public List<TemplateElement>? TemplateElement { get; set; }
+    }
+
+    public class MapTemplateDocument
+    {
+        public MapTemplate? MapTemplate { get; set; }
+    }
+
+    public class RandomlyPlacedThirdParty
+    {
+        public RandomlyPlacedThirdPartyValue? value { get; set; }
+    }
+
+    public class RandomlyPlacedThirdPartyValue
+    {
+        public short? id { get; set; }
+    }
+}
---
+++ Anno_FileDBModels/Anno1800/MapTemplate/TemplateElement.cs
@@ -0,0 +1,55 @@
+﻿using FileDBSerializing.EncodingAwareStrings;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Anno_FileDBModels.Anno1800.MapTemplate
+{
+    public class TemplateElement
+    {
+        public int? ElementType { get; set; }
+        public Element? Element { get; set; }
+    }
+
+    public class Element
+    {
+        // ElementType=0,1,2
+        public int[]? Position { get; set; }
+
+        // ElementType=0
+        public UnicodeString? MapFilePath { get; set; }
+        public byte? Rotation90 { get; set; }
+        public UTF8String? IslandLabel { get; set; }
+        public int[]? FertilityGuids { get; set; }
+        public bool? RandomizeFertilities { get; set; }
+        public List<Tuple<long, int>>? MineSlotMapping { get; set; }
+        public RandomIslandConfig? RandomIslandConfig { get; set; }
+
+        // ElementType=1
+        public short? Size { get; set; }
+        public Difficulty? Difficulty { get; set; }
+        public Config? Config { get; set; }
+    }
+
+    public class RandomIslandConfig
+    {
+        [SuppressMessage("Style", "IDE1006:Naming Styles")]
+        public Config? value { get; set; }
+    }
+
+    public class Config
+    {
+        public IslandType? Type { get; set; }
+        public Difficulty? Difficulty { get; set; }
+    }
+
+    public class IslandType
+    {
+        [SuppressMessage("Style", "IDE1006:Naming Styles")]
+        public short? id { get; set; }
+    }
+
+    public class Difficulty
+    {
+        [SuppressMessage("Style", "IDE1006:Naming Styles")]
+        public short? id { get; set; }
+    }
+}
